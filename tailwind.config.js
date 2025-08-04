6c4a6c36ea82c5c98b726d58a31ca0b5dc4c675d
ralphiezagha1-debug <ralphiezagha1@gmail.com>
2025-08-03 17:11:57 -0700

    Add tailwind.config.js for Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}",
+    "./pages/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
